39a0d659d932a5b27212bf20bcff8fba5ba20e0c
Steven Schroeder <schroederthecoder@gmail.com>
2016-04-26 11:10:10 -0500

    Removed the DBUtil class and re-added it. There were two versions floating around, DbUtil and DBUtil

---
+++ ConferenceWizard/src/java/com/javaee/conferencewizard/db/DBUtil.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.javaee.conferencewizard.db;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author Emmett
+ */
+public class DBUtil {
+    private static final EntityManagerFactory EMF =
+            Persistence.createEntityManagerFactory("ConferenceWizardPU");
+    public static EntityManagerFactory getEmFactory(){
+        return EMF;
+    }
+    
+    public static List<Person> getAttendees() {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        List<Person> attendees = em.createQuery("SELECT e FROM Person e").getResultList();
+        return attendees;
+    }
+}
